1f8c23f1aa3eae528a8817a362bf705a9b6a7194
Samara Leandro <samara.leandro2011@hotmail.com>
2022-11-17 19:42:11 -0300

    Realizando o requisito 18

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const botaoLogin = document.getElementById('botaoLogin');
 const email = document.getElementById('e-mail');
 const senha = document.getElementById('senha');
 
+const botaoEnviar = document.getElementById('submit-btn');
+const inputAgreement = document.getElementById('agreement');
+
 // Minhas Funções
 function validaUsuario () {
   if(email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -14,3 +17,16 @@ function validaUsuario () {
 
 // Minhas Chamadas
 botaoLogin.addEventListener('click', validaUsuario);
+
+// Desabilita o botão enviar:
+
+botaoEnviar.disabled = true;
+
+inputAgreement.addEventListener('click', ()=>{
+  if (botaoEnviar.disabled === true){
+    botaoEnviar.disabled = false;
+  } else {
+    botaoEnviar.disabled = true;
+  }
+})
+
