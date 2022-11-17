0b3d34bcdbe4e0c408d3cd1d5c1cf8b5fece7e3d
Caroline <carolinefernandesc@hotmail.com>
2022-11-17 20:37:26 -0300

    corrigindo javascript

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-
 const botaoLogin = document.getElementById('botaoLogin');
 const email = document.getElementById('e-mail');
 const senha = document.getElementById('senha');
 const botaoEnviar = document.getElementById('submit-btn');
 const inputAgreement = document.getElementById('agreement');
 
-
-function validaUsuario () {
+botaoLogin.addEventListener('click', () => {
   if(email.value === 'tryber@teste.com' && senha.value === '123456') {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Email ou senha inválidos.');
   }
-}
-
-botaoLogin.addEventListener('click', validaUsuario);
+});
 
 botaoEnviar.disabled = true;
 
-inputAgreement.addEventListener('click', ()=>{
+inputAgreement.addEventListener('click', () => {
   if (botaoEnviar.disabled === true){
     botaoEnviar.disabled = false;
   } else {
     botaoEnviar.disabled = true;
   }
 })
-
