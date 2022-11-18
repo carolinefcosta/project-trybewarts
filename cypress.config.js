593efe409653a249c6d7eab50b290297c88f5728
Caroline <carolinefernandesc@hotmail.com>
2022-11-18 08:54:23 -0300

    corrigindo style css

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter.json',
+  },
+  video: false,
+  screenshotOnRunFailure: false,
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 10000,
+}
